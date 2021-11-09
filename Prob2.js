14bb5a7970a46a87d6762badca8cd52e16c0796c
haesung8514 <82579445+haesung8514@users.noreply.github.com>
2021-11-09 18:13:47 +0900

    week2

---
+++ Prob2.js
@@ -0,0 +1,5 @@
+const a = parseInt(prompt("Input a"));
+const b = parseInt(prompt("Input b"));
+const c = parseInt(prompt("Input c"));
+console.log(a + b + c);
+document.write("<br />"+a+ " + " + b + " + " + c);
